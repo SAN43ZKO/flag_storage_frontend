3f5df6b901e173d8eae233f7ae64e4a24d413348
Alexandr <san43zko@gmail.com>
2026-05-25 16:08:55 +0200

    fix: collabora

--- src/pages/DocumentsPage.vue
+++ src/pages/DocumentsPage.vue
@@ -242,7 +242,7 @@ const dragOver = ref(false);
 const searchQuery = ref("");
 
 function editFile(file) {
-  const wopiHost = 'https://localhost'
+  const wopiHost = 'https://localhost:9980'
   const wopiSrc = encodeURIComponent(`${wopiHost}/wopi/files/${file.id}`)
   const editorUrl = `https://localhost:9980/browser/dist/cool.html?WOPISrc=${wopiSrc}`;
   window.open(editorUrl, "_blank");
