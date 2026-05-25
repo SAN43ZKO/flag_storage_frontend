5b5c95bbc51565462dc4a315d196554fb3c5856e
Alexandr <san43zko@gmail.com>
2026-05-25 16:09:34 +0200

    fix: collabora

--- src/pages/DocumentsPage.vue
+++ src/pages/DocumentsPage.vue
@@ -242,10 +242,10 @@ const dragOver = ref(false);
 const searchQuery = ref("");
 
 function editFile(file) {
-  const wopiHost = 'https://localhost:9980'
-  const wopiSrc = encodeURIComponent(`${wopiHost}/wopi/files/${file.id}`)
-  const editorUrl = `https://localhost:9980/browser/dist/cool.html?WOPISrc=${wopiSrc}`;
-  window.open(editorUrl, "_blank");
+    const base = 'https://localhost:9980'
+    const wopiSrc = encodeURIComponent(`${base}/wopi/files/${file.id}`)
+    const editorUrl = `${base}/browser/dist/cool.html?WOPISrc=${wopiSrc}`
+    window.open(editorUrl, '_blank')
 }
 
 let debounceTimer;
